d61f7cde99980bb470b57b3da7f3ab7ec2c45606
SenepartM <113522450+SenepartM@users.noreply.github.com>
2022-10-10 14:49:04 +0200

    Exercice 2 TP3 Tab
    occurence par la droite

--- Exercice2.c
+++ Exercice2.c
@@ -1,41 +1,29 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    double t[3][4], m[3], moy, moy1, moy2;
-    int i, j, c;
-    printf("entrer les 12 éléments du tableau");
+    char string[100];
+    char src;
+    int t, i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            scanf("%lf", &t[i][j]);
+    printf(" Entrez une chaîne de caractère :  ");
+    gets(string);
+    printf(" Saisir la lettre recherchée :  ");
+    scanf("%c", &src);
+    t = strlen(string);
+
+  
+       j=0;
+        for (i = t-1; i >= 0; i--){
+            j++;
+       
+        if(string[i]==src){
+        
+        printf("La lettre %c se situe en position %d en partant de la droite",src,j);
+    }
+            
         }
-    }
-
-    printf("LE TABLEAU A 2 DIMENSIONS :\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            printf("%.2lf\n", t[i][j]);
-        }
-        printf("\n");
-    }
-
-    moy = t[0][0] + t[0][1] + t[0][2] + t[0][3];
-    moy = moy / 4;
-    moy1 = t[1][0] + t[1][1] + t[1][2] + t[1][3];
-    moy1 = moy1 / 4;
-    moy2 = t[2][0] + t[2][1] + t[2][2] + t[2][3];
-    moy2 = moy2 / 4;
-
-    m[0] = moy;
-    m[1] = moy1;
-    m[2] = moy2;
-    for (c = 0; c < 3; c++)
-    {
-        printf("Moyenne de la ligne %d \n%lf\n", c + 1, m[c]);
-    }
+        
+    
+    return 0;
 }
