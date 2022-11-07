fd0c606d569d76d2ca2195a94e33da0a032e9268
SenepartM <113522450+SenepartM@users.noreply.github.com>
2022-11-07 15:10:10 +0100

    fonction affichage tab

---
+++ affichetab.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void affiche(int *t, int n);
+
+int main()
+{
+    int tab[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+    affiche(tab, 10);
+
+    return 0;
+}
+
+void affiche(int *t, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %d\n", i, t[i]);
+    }
+}
